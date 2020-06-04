da6a28c95fe7f1bf04699b5e298ef7d846a3a128
woomin <dal96k@gmail.com>
2020-06-04 14:28:37 +0900

    Modify about navigation

--- gatsby-meta-config.js
+++ gatsby-meta-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   title: `Woomin Blog`, // Your blog title
   description: `Jeon Woomin's Dev Blog`, // Your blog description
-  author: `Jeon Woomin`,
-  introduction: `takes notes of my trace`,
+  author: `About`,
+  introduction: `Jeon Woomin`,
   siteUrl: `http://woomin.netlify.app/`, // Your blog site url
   social: [
     //You can find and use icons on the https://fontawesome.com/icons?d=gallery&s=brands
