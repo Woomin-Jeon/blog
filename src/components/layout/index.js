60156b2d2231805cd96e2f9a9506d64f7580ea89
woomin <dal96k@gmail.com>
2020-06-17 12:04:15 +0900

    Add google analytics

--- src/components/layout/index.js
+++ src/components/layout/index.js
@@ -9,16 +9,6 @@ export const Layout = ({ children, title }) => (
   <>
     <Helmet>
       <meta name="google-site-verification" content="nqKk1qa0fFdufoYINoIqIgkDZOIFMyAOGir7cqK4BL8" />
-      
-      {/* Global site tag (gtag.js) - Google Analytics */}
-      <script async src="https://www.googletagmanager.com/gtag/js?id=UA-169806970-1"></script>
-      <script>
-        window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
-        gtag('js', new Date());
-
-        gtag('config', 'UA-169806970-1');
-      </script>
     </Helmet>
     <Top title={title} />
     <div className='layout-body'>
