0d231f9fc316f5dc8bd1f375d08eef228da7cd39
woomin <dal96k@gmail.com>
2020-06-18 09:34:32 +0900

    Add google-analytics-plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -89,7 +89,7 @@ module.exports = {
           {
             resolve: `gatsby-plugin-google-analytics`,
             options: {
-              trackingId: metaConfig.ga,
+              trackingId: process.env.GOOGLE_ANALYTICS_TRACKING_ID || "none",
             },
           },
           `gatsby-remark-smartypants`,
