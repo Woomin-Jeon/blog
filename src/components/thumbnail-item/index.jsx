bd0480c88048a4768485857cf51236a3979f40a7
Woomin-Jeon <dal96k@gmail.com>
2022-02-02 23:54:22 +0900

    fix: 블로그 UI 수정

--- src/components/thumbnail-item/index.jsx
+++ src/components/thumbnail-item/index.jsx
@@ -4,11 +4,14 @@ import { TARGET_CLASS } from '../../utils/visible'
 
 import './index.scss'
 
-export const ThumbnailItem = ({ node }) => (
+export const ThumbnailItem = ({ node }) => {
+  return (
   <Link className={`thumbnail ${TARGET_CLASS}`} to={node.fields.slug}>
     <div key={node.fields.slug}>
+      <span>{node.frontmatter.date}</span>
       <h3>{node.frontmatter.title || node.fields.slug}</h3>
       <p dangerouslySetInnerHTML={{ __html: node.excerpt }} />
     </div>
   </Link>
 )
+  }
